c81b079868098666a96416185e7accddd48903bb
Maarten Wong(veghel) <jachtschipwong@hotmail.com>
2019-08-31 18:49:15 +0200

    made some changes

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express')
 const app = express();
 const path = require("path");
 
+app.use('/', express.static(__dirname,'./index.css'));
+
 app.get('/', function(req, res){ 
   res.sendFile(path.join(__dirname, './index.html'));
 
 });
+
 
 app.listen(8000);
 console.log('Example app listening on port 8000!');

ae885da168cc818ace72237fa149c12c3a5f9919
Maarten Wong(veghel) <jachtschipwong@hotmail.com>
2019-08-31 17:55:28 +0200

    express now renders html

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const app = express();
-const path = require("path")
-
-app.use(express.static('public'));
+const path = require("path");
 
 app.get('/', function(req, res){ 
-  res.sendFile('index.html')
-})
+  res.sendFile(path.join(__dirname, './index.html'));
+
+});
 
 app.listen(8000);
 console.log('Example app listening on port 8000!');
